a19dc2a570dbcf93d08fb0eaaaa111fb71fc2708
Vedantkumar123 <vedantkumarvk123@gmail.com>
2025-09-15 17:39:18 +0530

    stuff

--- Architecture/model_architecture/poacher-2/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# poacher > 2024-09-13 8:42am
-https://universe.roboflow.com/poacher-ifyuf/poacher
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- WebUI/wss/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./templates/Homepage.css"; // ensure styles load
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
